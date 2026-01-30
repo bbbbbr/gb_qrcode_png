d6733a5f8e780c6996365455982d8780c4e417a2
bbbbbr <bbbbbr@users.noreply.github.com>
2026-01-29 23:32:56 -0800

    Convert process: split to separate source file

---
+++ src/img_2_qrcode.c
@@ -0,0 +1,101 @@
+#include <gbdk/platform.h>
+#include <gbdk/incbin.h>
+#include <gbdk/emu_debug.h>
+#include <stdint.h>
+
+#include <stdio.h>
+
+#include "common.h"
+
+#include "png_indexed.h"
+#include "base64.h"
+#include "qr_wrapper.h"
+
+
+#define PNG_OUT_BPP_1  1u
+#define PNG_OUT_BPP_2  2u
+#define PNG_OUT_BPP_4  4u
+#define PNG_OUT_BPP_8  8u
+
+
+// ===== START PNG TEST IMAGE =====
+#define IMG_8X8_4_COLORS_8BPP_ENCODED_WIDTH  8u
+#define IMG_8X8_4_COLORS_8BPP_ENCODED_HEIGHT 8u
+
+const uint8_t img_8x8_4_colors_8bpp_encoded_pal[] = {
+    0x00u, 0xFFu, 0x00u,  // Green
+    0xFFu, 0xFFu, 0xFFu,  // White
+    0x00u, 0x00u, 0xFFu,  // Blue
+    0xFFu, 0x00u, 0x00u,  // Red
+};
+const uint8_t img_8x8_4_colors_8bpp_encoded_map[] = {
+     // 0x78u, 0x01u, 
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x02u, 0x03u, 0x00u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x00u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u,
+     /* 0x01u, 0x09u, 0x00u, 0xF6u, 0xFFu, 0x00u, */  0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x01u, 0x03u,
+     // 0x09u, 0xD8u, 0x00u, 0x45u, 
+};
+
+// ===== END PNG TEST IMAGE =====
+
+// Use SRAM for temp buffers
+const uint8_t * SRAM_base  = 0xA000u;
+const uint8_t * SRAM_upper = 0xB000u;
+
+
+
+void image_to_png_qrcode_url(void) {
+
+    SWITCH_RAM(0u); // RAM bank 0
+
+    // Output buffers in Cart SRAM, no need to allocate them
+    uint8_t * p_png_buf            = SRAM_base;
+    uint8_t * p_base64_png_url_str = SRAM_upper;
+
+
+    // ===== Conversion to PNG =====
+    printf("Generating PNG\n");
+    uint16_t png_buf_sz = png_indexed_init(IMG_8X8_4_COLORS_8BPP_ENCODED_WIDTH,
+                                           IMG_8X8_4_COLORS_8BPP_ENCODED_HEIGHT, 
+                                           // PNG_OUT_BPP_8,     // Current build works, to match  test_8x8_indexed_nocomp_2bpp-encoded.png use 8BPP
+                                           PNG_OUT_BPP_2,        // Output passes pngcheck and imports to GIMP ok
+                                           ARRAY_LEN(img_8x8_4_colors_8bpp_encoded_pal));
+
+    png_indexed_set_buffers(img_8x8_4_colors_8bpp_encoded_pal,
+                            img_8x8_4_colors_8bpp_encoded_map,
+                            p_png_buf);
+
+    uint16_t png_file_output_sz = png_indexed_encode();
+    EMU_printf("PNG out sz=%u\n", png_file_output_sz);
+
+
+    // ===== PNG encoding to Base64 URL =====
+    printf("Base 64 Encode PNG\n");
+    uint16_t b64_enc_len = base64_encode_to_url(p_base64_png_url_str, p_png_buf, png_file_output_sz);
+
+    EMU_printf("B64 out sz=%u\n", (uint16_t)b64_enc_len);
+    EMU_printf("B64 out:%s\n", p_base64_png_url_str);
+
+    
+    // TODO: move buffer to SRAM? Possibly overwrite previously generated PNG file output
+
+    // ===== PNG encoding to Base64 URL =====
+    // QR Code is generated in byte mode because:
+    // - Ported C implementation doesn't support other modes
+    // - Alphanumeric mode character set doesn't include all chars needed for base64 encoded strings and mime header chars (;)
+    //
+    EMU_printf("Generating QR Code\n");
+    printf("Generating QR Code\n");    
+    if (qr_generate(p_base64_png_url_str, b64_enc_len) ) {
+        EMU_printf("Rendering QR Code\n");
+        qr_render();
+    } else {
+        EMU_printf("QR Code gen Error\n");
+    }
+
+}
---
+++ src/img_2_qrcode.h
@@ -0,0 +1,6 @@
+#ifndef IMG_2_QRCODE_H
+#define IMG_2_QRCODE_H
+
+void image_to_png_qrcode_url(void);
+
+#endif // IMG_2_QRCODE_H
